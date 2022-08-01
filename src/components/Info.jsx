20f8f06667e930f7f81d457aa05a925da4a9ef61
Brian <briandave771@gmail.com>
2022-08-01 04:06:17 +0300

    added changes

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import MeImage from "../assets/me.png"
-import Mail from "../assets/mail.svg"
 import LinkedIn from "../assets/linkedin.svg"
 
  function Info() {
@@ -11,7 +10,7 @@ import LinkedIn from "../assets/linkedin.svg"
             <h3 className="info--title">Frontend Developer</h3>
             <h3 className="info--email">briandave771@gmail.com</h3>
             <a href="#" className="btn info--btn_email">
-                <img src={Mail} className="img--svg"/>Email</a>
+                Email</a>
             <a href="#" className="btn info--btn_linkedin">
                 <img src={LinkedIn} className="img--svg"/>LinkedIn</a>
     
